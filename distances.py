2d3e30435a346ab9067b9a627c26dcca3bf2432e
Monica Baluna <monicamariabaluna@gmail.com>
2018-03-01 21:32:10 +0200

    kajdow

--- distances.py
+++ distances.py
@@ -4,6 +4,13 @@ import argparse
 
 from matplotlib import pyplot
 
+class Ride:
+    def __init__(self, start, end, earliest, latest):
+        self.start = start
+        self.end = end
+        self.earliest = earliest
+        self.latest = latest
+
 def manhattan_distance(a, b):
     al, ac = a
     bl, bc = b
@@ -23,15 +30,23 @@ if __name__ == "__main__":
     starts = []
     ends = []
     num = 0
+    rides = []
 
     for line in args.in_file:
     	params = [int(x) for x in line.strip().split(" ")]
+
     	start = (params[0], params[1])
     	end = (params[2], params[3])
+        earliest, latest = (params[4], params[5])
+
+        rides.append(Ride(start, end, earliest, latest))
+
     	distances.append(manhattan_distance(start, end))
     	starts.append(start)
     	ends.append(end)
     	num += 1
 
-    pyplot.plot([s[0] for s in starts], [s[1] for s in starts], 'o')
+    # start_times = [r.earliest for r in rides if r.start[0] <= 5000]
+    pyplot.plot([r.start[0] for r in rides], [r.start[1] for r in rides], 'o')
+    pyplot.plot([r.end[0] for r in rides], [r.end[1] for r in rides], 'ro')
     pyplot.show()
